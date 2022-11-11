06cfd9a69cd024401e77a89dcf27ed4071e248eb
divinojack <115471906+divinojack@users.noreply.github.com>
2022-11-11 15:44:31 +0100

    Create main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(){
 //Qui vanno definite le variabili
-
+int main()
+{
+    float a=0, b=0, c=0;
+    cin >> a >> b >> c;
 cin >> a >> b >> c;
 
 //Qui va scritto il codice per eseguire i calcoli richiesti
